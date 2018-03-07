7abe83af60383a8a4dc884b22005556f9dcded1b
Yang Feng <yyy333ddd333@hotmail.com>
2018-03-07 18:44:52 +0800

    学到 webpack 全局资源

--- webpack/webpack-demo/src/index.js
+++ webpack/webpack-demo/src/index.js
@@ -1,10 +1,19 @@
 import _ from 'lodash'
+import "./style.css"
+import MyImage from "./01.svg"
+import Data from './data.xml'
 
 function component() {
     var element = document.createElement('div');
   
     element.innerHTML = _.join(['Hello', 'webpack'], ' ^.^ ');
-  
+    element.classList.add("hello")
+    //var myImage = new Image()
+    //myImage.src = MyImage
+    //element.appendChild(myImage)
+    //
+    console.log(Data)
+
     return element;
   }
   
