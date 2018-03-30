23207bdf9db9f94017bdc2fd16c25ed784456099
Yang Feng <yyy333ddd333@hotmail.com>
2018-03-30 19:35:32 +0800

    save

--- webpack/webpack-demo/webpack.config.js
+++ webpack/webpack-demo/webpack.config.js
@@ -1,10 +1,10 @@
 const path = require('path');
-const webpack = require('webpack')
+//const webpack = require('webpack')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
   entry: {
-    lib: './src/index.js',
+    lib: './src/index.ts',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -15,21 +15,29 @@ module.exports = {
     new CleanWebpackPlugin(['dist'], {
       exclude: 'index.html'
     }),
-    new webpack.ProvidePlugin({
-      _: 'lodash'
-    })
+//    new webpack.ProvidePlugin({
+//      _: 'lodash'
+//    })
   ],
   module: {
     rules: [
       {
-        test: require.resolve('./src/globals.js'),
-        use: 'exports-loader?file,parse=helpers.parse'
-      },
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+//      {
+//        test: require.resolve('./src/globals.js'),
+//        use: 'exports-loader?file,parse=helpers.parse'
+//      },
 //      {
 //        test: require.resolve('./src/index.js'),
 //        use: 'imports-loader?this=>window'
 //      },
     ]
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ]
   },
 //  externals: {
 //    lodash: {
