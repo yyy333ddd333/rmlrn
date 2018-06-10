80a9f2e613873f2c9b4cc4176e6bfb7e03978ef3
Yang Feng <yyy333ddd333@hotmail.com>
2018-06-10 23:00:08 +0800

    create test01

---
+++ test01/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <input v-model="shareNo" />
+    <button @click="getInfo">查询</button>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import jsonp from 'jsonp'
+
+export default {
+  name: 'app',
+  data: function(){
+    return {
+      shareNo: "sh601009",
+    }
+  },
+  methods: {
+    getInfo() {
+      // axios.get('http://web.juhe.cn:8080/finance/stock/hs?gid=sh601009&key=b8b2dbe7bebf93ed84842bce7713f452')
+      // .then(res => console.log(res.data))
+      // jsonp('http://web.juhe.cn:8080/finance/stock/hs?gid=sh601009&key=b8b2dbe7bebf93ed84842bce7713f452')
+    }
+  },
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
