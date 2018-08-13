6be85a8f170190dbb59d8b67f9506658347b9f25
Yang Feng <yyy333ddd333@hotmail.com>
2018-08-13 22:26:29 +0800

    学习fortran中的数组

--- fortran/lrn_pc_01/main.f90
+++ fortran/lrn_pc_01/main.f90
@@ -1,3 +1,15 @@
 program main
-    write(*,*) "hello fortran"
+    implicit none
+    integer :: i,j
+    integer, dimension(5, 5) :: nums
+    integer, dimension(25) :: nums2
+    integer, dimension(5, 5) :: nums3
+    nums2 = (/(i, i=1, 25)/)
+    write(*,*) nums2
+    nums = reshape((/(i, i=1,25)/), (/5,5/))
+    write(*,100) ((nums(i,j), j=1,5), i=1,5)
+    nums3 = reshape((/((5*i+j, i=0,4), j=1,5)/), (/5,5/))
+    write(*,100) ((nums3(i,j), j=1,5), i=1,5)
+    100 format(1x, 5i8)
+    write(*,*) (nums3(1:3, 1:5:2))
 end program main
