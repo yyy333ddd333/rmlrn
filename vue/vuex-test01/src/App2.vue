95a9aaa3c4cb12af45252896091f8d5371e0f4aa
yangfeng <yyy333ddd333@hotmail.com>
2018-12-04 13:49:56 +0800

    新建app2

---
+++ vue/vuex-test01/src/App2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id='app2'>
+    <button type="button" v-on:click="inc">+</button>  
+  </div>
+</template>
+
+<script>
+//import store from './store.js'
+export default{
+  methods: {
+    inc: function(){console.log('inc')}
+  }  
+}
+</script>
--- vue/vuex-test01/src/main.js
+++ vue/vuex-test01/src/main.js
@@ -1,10 +1,20 @@
+import VConsole from 'vconsole'
+var vConsole = new VConsole()
+
 import Vue from 'vue'
 import App from './App.vue'
+import App2 from './App2.vue'
 import store from './store'
 
 Vue.config.productionTip = false
-
+/*
 new Vue({
   store,
   render: h => h(App)
 }).$mount('#app')
+*/
+new Vue({
+  render: h => h(App2)
+}).$mount('#app')
+//store.commit('increment')
+//console.log(store.state.count)
