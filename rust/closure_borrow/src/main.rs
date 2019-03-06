c4c83c3921f4dd142ffafdca3ce11c78207ddf1e
yangfeng <yyy333ddd333@hotmail.com>
2019-03-07 00:12:26 +0800

    添加注释解释了spawn创建线程中闭包参数必须move环境中的变量的原因

--- rust/closure_borrow/src/main.rs
+++ rust/closure_borrow/src/main.rs
@@ -6,6 +6,7 @@ fn main() {
     //let v_ref = &vec![4, 5, 6];
     //println!("{:?}", v_ref);
     
+    //这里用move是因为如果不用move，printlin!会借用&v作为参数，导致生命周期和主函数的生命周期一致。但是spawn()要求闭包参数的生命周期是'static
     let handle = thread::spawn(move || {
         //2 * v2
         //println!("{:?}", v2);
