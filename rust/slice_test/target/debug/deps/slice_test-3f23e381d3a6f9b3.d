8529b300d34f4bb356807d66e1a0b3dd2cf22586
Yang Feng <yyy333ddd333@hotmail.com>
2019-04-21 14:43:35 +0800

    删除target，这个文件夹不应该commit

--- rust/slice_test/target/debug/deps/slice_test-3f23e381d3a6f9b3.d
+++
@@ -1,5 +0,0 @@
-/data/data/com.termux/files/home/rmlrn/rust/slice_test/target/debug/deps/slice_test-3f23e381d3a6f9b3: src/main.rs
-
-/data/data/com.termux/files/home/rmlrn/rust/slice_test/target/debug/deps/slice_test-3f23e381d3a6f9b3.d: src/main.rs
-
-src/main.rs:
